65eb29e4fec9df0f9bddaea9b372f991b3fa4ee3
Jason Hee <jasonheecs@gmail.com>
2016-12-07 18:28:55 +0800

    Modified execAsUser to use bash instead of Bourne shell

--- setupLibrary.sh
+++ setupLibrary.sh
@@ -41,7 +41,7 @@ function execAsUser() {
     local username=${1}
     local exec_command=${2}
 
-    sudo -u "${username}" -H sh -c "${exec_command}"
+    sudo -u "${username}" -H bash -c "${exec_command}"
 }
 
 # Modify the sshd_config file
