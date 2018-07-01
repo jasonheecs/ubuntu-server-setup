3aefceafec8f0017ed50ffc210c535cf422eb925
Martinus Suherman <martinus.suherman@gmail.com>
2018-07-01 04:21:25 +0000

    Add docker install function

--- setupLibrary.sh
+++ setupLibrary.sh
@@ -145,3 +145,10 @@ function revertSudoers() {
     sudo cp /etc/sudoers.bak /etc/sudoers
     sudo rm -rf /etc/sudoers.bak
 }
+
+function installDocker() {
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+}
