1d3842a38e75536db5aea084c3fa1ff181bb7dbc
Martinus Suherman <martinus.suherman@gmail.com>
2018-07-01 04:25:13 +0000

    Add comment for docker install function

--- setupLibrary.sh
+++ setupLibrary.sh
@@ -146,6 +146,8 @@ function revertSudoers() {
     sudo rm -rf /etc/sudoers.bak
 }
 
+# Install docker
+# from: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
 function installDocker() {
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
