c1746eaead70f2f07ac7145cc842a7d72f3320f9
Martinus Suherman <martinus.suherman@gmail.com>
2018-07-01 05:02:29 +0000

    Add docker compose install function

--- setupLibrary.sh
+++ setupLibrary.sh
@@ -161,3 +161,10 @@ function installDocker() {
     sudo apt-get update
     sudo apt-get install -y docker-ce
 }
+
+# Install docker compose
+# reference: https://docs.docker.com/compose/install/
+function installDockerCompose() {
+    sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+}
