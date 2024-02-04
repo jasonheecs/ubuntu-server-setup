2a063eea882abb6282a2355ad2858f4e5a8153d1
Fab <fhulous@gmail.com>
2024-02-04 19:47:56 +0100

    set timezone default to 'Europe/Paris'.

--- setup.sh
+++ setup.sh
@@ -89,10 +89,10 @@ function logTimestamp() {
 }
 
 function setupTimezone() {
-    echo -ne "Enter the timezone for the server (Default is 'Asia/Singapore'):\n" >&3
+    echo -ne "Enter the timezone for the server (Default is 'Europe/Paris'):\n" >&3
     read -r timezone
     if [ -z "${timezone}" ]; then
-        timezone="Asia/Singapore"
+        timezone="Europe/Paris"
     fi
     setTimezone "${timezone}"
     echo "Timezone is set to $(cat /etc/timezone)" >&3
